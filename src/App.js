1933b62baafc89413ebc427f1d5b1a991e918234
CodingEssentials007 <uzairmemon4991@gmail.com>
2022-09-18 03:15:21 -0700

    fetchapi

--- src/App.js
+++ src/App.js
@@ -1,107 +1,26 @@
 import './App.css';
-import CustomButton from './Component/CustomButton';
-import CustomInput from './Component/CustomInput';
-import Header from './Component/Header';
-import Card from './Component/Card';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-
-const [add, setAdd]=useState(false);
-const [Tasks, setTasks]=useState([]);
-
-const [singleTask, setSingleTask]=useState('');
-const [singleDes, setSingleDes]=useState('');
-
-const UpdateTask =(id)=>
-{
-    setTasks(
-        Tasks.map((t)=>(
-            t.id==id? {...t,complete:true}:t
-        ))
-    )
-
-}
-const deleteTask =(id)=>
-{
-    setTasks(
-        Tasks.filter((t)=>(
-            t.id==id?false:true
-        ))
-    )
-
-}
-
-const addToCard =()=>
-{
-    const id= Tasks.length==0?1:Tasks.length+1;
-    const taskDetail=
-    {
-        id:id,
-        task:singleTask,
-        des:singleDes,
-        complete:false
-
-    }
-    setTasks(
-        [...Tasks,taskDetail]
-    )
-
-}
-
-const ClearInput =()=>
-{
-    setSingleTask('');
-    setSingleDes('');
     
-}
+    const [img,setImg]=useState('');
+   
+    const handleCall=()=>
+    {
+        axios.get("https://dog.ceo/api/breeds/image/random")
+        .then((res)=>(setImg(res.data.message)))
+    }
+    useEffect(()=>{
+    axios.get("https://dog.ceo/api/breeds/image/random")
+    .then((res)=>(setImg(res.data.message)))
+    },[])
 
-
-const handleCustomTask =(event)=>
-{
-    setSingleTask(event.target.value);
-}
-
-const handleCustomDes =(event)=>
-{
-    setSingleDes(event.target.value);
-}
-
-
-
-const handleInput =()=>
-{
-    setAdd(!add)
-}
-
-
+   
     return ( 
-        <div className='main'>
-            <div className='inputSection'>
-               <Header handleInput={handleInput}  />
-                {add==true ?
-                    <>
-                    <CustomInput  value={singleTask}  placeHolder='Enter Task' name='Task' change={handleCustomTask}  /> 
-                    <CustomInput   value={singleDes} placeHolder='Enter Description' name='Description' change={handleCustomDes}  />
-                    <div className='btnwrapper'>
-                    <CustomButton color='White' bg='#1877F2'  name='Save Task' click={addToCard} />
-                    <CustomButton color='White' bg='red'  name='Cancle'   click={ClearInput} />
-                </div>
-                    </>:null
-                }
-               
-               
-            </div>
-
-            <div className='cardSection'>
-               {
-                Tasks.map((t)=>
-                ( 
-                    <Card title={t.task} des={t.des}   key={t.id} delete={()=>deleteTask(t.id)}  
-                       update={()=>UpdateTask(t.id)}  complete={t.complete}   />
-                ))
-               }
-            </div>
+        <div >
+           <img  src={img}  style={{width:'20vw', height:'40vh'}}  />
+            <button onClick={handleCall}>click</button>      
         </div>
      );
 }
